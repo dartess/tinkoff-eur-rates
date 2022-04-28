2ecaae885a921fdba0325ec8529a49bc0e0dc158
Sergey Kozlov <dartess@mail.ru>
2022-04-28 18:18:08 +0300

    add route for only price value

--- index.js
+++ index.js
@@ -9,19 +9,19 @@ const prices = [];
 const MAX_ITEMS = 24 * 60;
 const INTERVAL = 20 * 1000;
 
-setInterval(
-    async () => {
-        const date = Date.now();
-        let price = -1;
-        try {
-            const { payload: { rates } } = await got.get('https://api.tinkoff.ru/v1/currency_rates?from=EUR&to=RUB').json();
-            price = rates.find(rate => rate.category === 'OPSRateGroup').buy;
-        } catch {}
-        prices.unshift({ date, price });
-        prices.length = Math.min(MAX_ITEMS, prices.length);
-    },
-    INTERVAL
-)
+const updateRate = async () => {
+    const date = Date.now();
+    let price = -1;
+    try {
+        const { payload: { rates } } = await got.get('https://api.tinkoff.ru/v1/currency_rates?from=EUR&to=RUB').json();
+        price = rates.find(rate => rate.category === 'OPSRateGroup').buy;
+    } catch {}
+    prices.unshift({ date, price });
+    prices.length = Math.min(MAX_ITEMS, prices.length);
+};
+
+updateRate();
+setInterval(updateRate, INTERVAL);
 
 router.get('/all', (ctx, next) => {
     ctx.body = prices;
@@ -30,6 +30,11 @@ router.get('/all', (ctx, next) => {
 
 router.get('/last', (ctx, next) => {
     ctx.body = prices[0];
+    return next();
+});
+
+router.get('/last-only-price', (ctx, next) => {
+    ctx.body = prices[0].price;
     return next();
 });
 
