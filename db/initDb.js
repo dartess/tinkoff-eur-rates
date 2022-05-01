dca6ad25902dba98ac4080124807049657227647
Sergey Kozlov <dartess@mail.ru>
2022-05-01 14:44:35 +0300

    fix unlink

--- db/initDb.js
+++ db/initDb.js
@@ -3,7 +3,9 @@ import {promisify} from 'util';
 import {getDb} from "./getDb.js";
 import {DB_FILE} from "./constants.js";
 
-await promisify(fs.unlink)(DB_FILE);
+try {
+    await promisify(fs.unlink)(DB_FILE);
+} catch {}
 await promisify(fs.writeFile)(DB_FILE, '');
 const db = await getDb();
 await db.exec('CREATE TABLE rates (date int, price int)');
